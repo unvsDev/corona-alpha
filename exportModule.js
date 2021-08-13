4305bced57b2a860c423a46be6cbc5c5486f2803
Minseo Kang <63099769+unvsDev@users.noreply.github.com>
2021-08-13 20:58:07 +0900

    Update exportModule.js

--- exportModule.js
+++ exportModule.js
@@ -1,4 +1,4 @@
-let overview; var currentCnt; var currentGap; var totalGap;
+let overview; var currentCnt; var currentGap; var totalGap; var resDate;
   
 const getData = async () => {
   const dataURL = "https://apiv2.corona-live.com/stats.json"
@@ -7,9 +7,26 @@ const getData = async () => {
 
   currentCnt = overview["current"][0]
   currentGap = overview["current"][1]
-  totalGap = overview["confirmed"][1]
-  totalGap = 1990 // Temporarily patch (2021/8/13)
+  // totalGap = overview["confirmed"][1]
   
+  const dataURL2 = 'http://ncov.mohw.go.kr'
+  let webView = new WebView()
+  await webView.loadURL(dataURL2)
+
+  let covid = await webView.evaluateJavaScript(`
+      const baseSelector = 'div.mainlive_container div.liveboard_layout '
+      let date = document.querySelector(baseSelector + 'h2 span.livedate').innerText
+      let domestic = document.querySelector(baseSelector + 'div.liveNum_today_new ul li:nth-child(1) span.data').innerText
+      let overseas = document.querySelector(baseSelector + 'div.liveNum_today_new ul li:nth-child(2) span.data').innerText
+
+      completion({date, count: {
+          domestic: domestic.replace(",", ""), overseas
+      }, wpsData: WPS_data })
+  `, true)
+
+  totalGap = parseInt(covid.count.domestic) + parseInt(covid.count.overseas)
+  resDate = covid.date.replace(/\(|\)/g, '').split(',')[0]
+    
   return [currentCnt, currentGap, totalGap]
 }
 
